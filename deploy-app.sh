aab68b68b2342c4f4d216cd064624d14101c2ae4
Bryce Klinker <bklinker@pillartechnology.com>
2019-05-28 13:18:25 -0500

    Added logging to deployment

--- deploy-app.sh
+++ deploy-app.sh
@@ -24,31 +24,42 @@ login_to_azure() {
 }
 
 create_resource_group_if_not_exists() {
+    echo "Checking that resource group ${RESOURCE_GROUP_NAME} exists.."
     EXISTS=$(az group exists -n "${RESOURCE_GROUP_NAME}" | jq length)
 
     if [ "${EXISTS}" = "false" ]; then 
+        echo "Creating resource group ${RESOURCE_GROUP_NAME}..."
         az group create -l "${LOCAITON}" -n "${RESOURCE_GROUP_NAME}"
+        echo "Created resource group ${RESOURCE_GROUP_NAME}."
     fi
 }
 
 create_storage_account_if_not_exists() {
+    echo "Checking that storage account ${STORAGE_ACCOUNT_NAME} exists..."
     EXISTS=$(az storage account show -g "${RESOURCE_GROUP_NAME}" -n "${STORAGE_ACCOUNT_NAME}")
 
     if [ $EXISTS = 1 ]; then
+        echo "Creating storage account ${STORAGE_ACCOUNT_NAME}..."
         az storage account create -n "${STORAGE_ACCOUNT_NAME}" -g "${RESOURCE_GROUP_NAME}" -l "${LOCAITON}"
+        echo "Created storage account ${STORAGE_ACCOUNT_NAME}."
     fi
 }
 
 create_function_app_if_not_exists() {
+    echo "Checking if function app ${FUNCTION_APP_NAME}..."
     EXISTS=$(az functionapp show --name "${FUNCTION_APP_NAME}" --resource-group "${RESOURCE_GROUP_NAME}")
 
     if [ $EXISTS = 1 ]; then
+        echo "Creating function app ${FUNCTION_APP_NAME}..."
         az funcitonapp create --consumption_plan-location "${LOCATION}" --name "${FUNCTION_APP_NAME}" --os-type Windows --resource-group "${RESOURCE_GROUP_NAME}" --runtime dotnet --storage-account "${STORAGE_ACCOUNT_NAME}"
+        echo "Created function app ${FUNCTION_APP_NAME}."
     fi
 }
 
 deploy_function_app() {
+    echo "Deploying to ${FUNCTION_APP_NAME}..."
     az functionapp deployment source config-zip -g "${RESOURCE_GROUP_NAME}" -n "${FUNCTION_APP_NAME}" --src "${ARCHIVED_FUNCTION_PATH}"
+    echo "Deployed to ${FUNCTION_APP_NAME}."
 }
 
 main
