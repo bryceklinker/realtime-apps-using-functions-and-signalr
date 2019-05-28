6ae56e894bcc0376f8c1d70d95692c52cd184905
Bryce Klinker <bklinker@pillartechnology.com>
2019-05-28 13:14:40 -0500

    Adding quotes around variable usages

--- deploy-app.sh
+++ deploy-app.sh
@@ -18,22 +18,24 @@ main() {
 }
 
 login_to_azure() {
-    az login --username ${AZURE_USERNAME} --password ${AZURE_PASSWORD}
+    echo 'Logging in to azure...'
+    az login --username "${AZURE_USERNAME}" --password "${AZURE_PASSWORD}"
+    echo 'Finished logging into azure.'
 }
 
 create_resource_group_if_not_exists() {
     EXISTS=$(az group exists -n ${RESOURCE_GROUP_NAME} | jq length)
 
     if [ "${EXISTS}" = "false" ]; then 
-        az group create -l ${LOCAITON} -n ${RESOURCE_GROUP_NAME}
+        az group create -l "${LOCAITON}" -n "${RESOURCE_GROUP_NAME}"
     fi
 }
 
 create_storage_account_if_not_exists() {
-    EXISTS=$(az storage account show -g ${RESOURCE_GROUP_NAME} -n ${STORAGE_ACCOUNT_NAME})
+    EXISTS=$(az storage account show -g "${RESOURCE_GROUP_NAME}" -n "${STORAGE_ACCOUNT_NAME}")
 
     if [ $EXISTS = 1 ]; then
-        az storage account create -n ${STORAGE_ACCOUNT_NAME} -g ${RESOURCE_GROUP_NAME} -l ${LOCAITON}
+        az storage account create -n "${STORAGE_ACCOUNT_NAME}" -g "${RESOURCE_GROUP_NAME}" -l "${LOCAITON}"
     fi
 }
 
@@ -41,12 +43,12 @@ create_function_app_if_not_exists() {
     EXISTS=$(az functionapp show --name "${FUNCTION_APP_NAME}" --resource-group "${RESOURCE_GROUP_NAME}")
 
     if [ $EXISTS = 1 ]; then
-        az funcitonapp create --consumption_plan-location ${LOCATION} --name ${FUNCTION_APP_NAME} --os-type Windows --resource-group ${RESOURCE_GROUP_NAME} --runtime dotnet --storage-account ${STORAGE_ACCOUNT_NAME}
+        az funcitonapp create --consumption_plan-location "${LOCATION}" --name "${FUNCTION_APP_NAME}" --os-type Windows --resource-group "${RESOURCE_GROUP_NAME}" --runtime dotnet --storage-account "${STORAGE_ACCOUNT_NAME}"
     fi
 }
 
 deploy_function_app() {
-    az functionapp deployment source config-zip -g ${RESOURCE_GROUP_NAME} -n ${FUNCTION_APP_NAME} --src "${ARCHIVED_FUNCTION_PATH}"
+    az functionapp deployment source config-zip -g "${RESOURCE_GROUP_NAME}" -n "${FUNCTION_APP_NAME}" --src "${ARCHIVED_FUNCTION_PATH}"
 }
 
 main
