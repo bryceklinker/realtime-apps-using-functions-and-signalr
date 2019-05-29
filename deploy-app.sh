aa3b42b2535bea487af9e3de44d8e9cb92af4010
Bryce Klinker <bklinker@pillartechnology.com>
2019-05-29 08:31:21 -0500

    Adding required infrastructure for application to work

--- deploy-app.sh
+++ deploy-app.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
-set -ex
+set -e
+
+brew install jq
 
 LOCATION='centralus'
 RESOURCE_GROUP_NAME='realtime-app-rg'
 FUNCTION_APP_NAME='realtime-app-func'
 STORAGE_ACCOUNT_NAME='realtimestorage'
+STORAGE_ACCOUNT_CONNECTION_STRING=''
+
+SIGNALR_NAME='realtime-app-signalr'
+SIGNALR_KEY=''
+SIGNALR_ENDPOINT="https://${SIGNALR_NAME}.service.signalr.net"
 
 main() {
     create_resource_group_if_not_exists
     create_storage_account_if_not_exists
+    create_signalr_if_not_exists
     create_function_app_if_not_exists
+    update_function_app_settings
     deploy_function_app
 }
 
@@ -39,17 +48,41 @@ create_storage_account_if_not_exists() {
         az storage account create -n "${STORAGE_ACCOUNT_NAME}" -g "${RESOURCE_GROUP_NAME}" -l "${LOCAITON}"
         echo "Created storage account ${STORAGE_ACCOUNT_NAME}."
     fi
+
+    STORAGE_ACCOUNT_CONNECTION_STRING=$(az storage account show-connection-string -g "${RESOURCE_GROUP_NAME}" -n "${STORAGE_ACCOUNT_NAME}" | jq .connectionString)
+}
+
+create_signalr_if_not_exists() {
+    echo "Checking that signalr service ${SIGNALR_NAME} exists..."
+    EXISTS=$(az signalr show -n ${SIGNALR_NAME} -g ${RESOURCE_GROUP_NAME})
+
+    if [ $EXISTS = 1 ]; then
+        echo "Creating signalr ${SIGNALR_NAME}..."
+        az signalr create -n "${SIGNALR_NAME}" -g "${RESOURCE_GROUP_NAME}" --sku "Free_DS2" --location "${LOCATION}"
+        echo "Created signalr ${SIGNALR_NAME}."
+    fi
 }
 
 create_function_app_if_not_exists() {
     echo "Checking if function app ${FUNCTION_APP_NAME}..."
-    EXISTS=$(az functionapp show --name "${FUNCTION_APP_NAME}" --resource-group "${RESOURCE_GROUP_NAME}")
+    EXISTS=$(az functionapp show -n "${FUNCTION_APP_NAME}" -g "${RESOURCE_GROUP_NAME}")
 
     if [ $EXISTS = 1 ]; then
         echo "Creating function app ${FUNCTION_APP_NAME}..."
-        az funcitonapp create --consumption_plan-location "${LOCATION}" --name "${FUNCTION_APP_NAME}" --os-type Windows --resource-group "${RESOURCE_GROUP_NAME}" --runtime dotnet --storage-account "${STORAGE_ACCOUNT_NAME}"
+        az funcitonapp create --consumption_plan-location "${LOCATION}" -n "${FUNCTION_APP_NAME}" --os-type Windows -g "${RESOURCE_GROUP_NAME}" --runtime dotnet --storage-account "${STORAGE_ACCOUNT_NAME}"
         echo "Created function app ${FUNCTION_APP_NAME}."
     fi
+
+    SIGNALR_KEY=$(az signalr key list -n "${SIGNALR_NAME}" -g "${RESOURCE_GROUP_NAME} | jq .primaryKey")
+}
+
+update_function_app_settings() {
+    az functionapp config appsettings set -n "${FUNCTION_APP_NAME}" -g "${RESOURCE_GROUP_NAME}" --settings "AzureWebJobsStorage=${STORAGE_ACCOUNT_CONNECTION_STRING}"
+    az functionapp config appsettings set -n "${FUNCTION_APP_NAME}" -g "${RESOURCE_GROUP_NAME}" --settings "StorageAccountConnectionString=${STORAGE_ACCOUNT_CONNECTION_STRING}"
+    az functionapp config appsettings set -n "${FUNCTION_APP_NAME}" -g "${RESOURCE_GROUP_NAME}" --settings "SignalR:Endpoint=${SIGNALR_ENDPOINT}"
+    az functionapp config appsettings set -n "${FUNCTION_APP_NAME}" -g "${RESOURCE_GROUP_NAME}" --settings "SignalR:Key=${SIGNALR_KEY}"
+
+    az functionapp cors add -n "${FUNCTION_APP_NAME}" -g "${RESOURCE_GROUP_NAME}" --allowed-origins "*"
 }
 
 deploy_function_app() {
