07162d0e2ba4bef7f7a47974a4697824faf5837c
Bryce Klinker <bryce.klinker@gmail.com>
2020-04-07 15:08:31 -0500

    Updating to use hash as part of the zip file name

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,15 +2,18 @@
 
 set -ex
 
+export HASH=$(git rev-parse HEAD)
 export GITHUB_WORKSPACE=${GITHUB_WORKSPACE:-$(pwd)}
 export FUNCTION_APP_PATH="${GITHUB_WORKSPACE}/src/Realtime.Presenter.Function"
 export INFRASTRUCTURE_PATH="${GITHUB_WORKSPACE}/src/Realtime.Presenter.Infrastructure"
+export FUNCTION_BUILD_PATH="${INFRASTRUCTURE_PATH}/build/function_app"
+export FUNCTION_ZIP_PATH="${INFRASTRUCTURE_PATH}/build/function_app_${HASH}.zip"
 
 build_function_app() {
-    dotnet publish ${FUNCTION_APP_PATH} --configuration Release --output "${INFRASTRUCTURE_PATH}/build/function_app"
+    dotnet publish ${FUNCTION_APP_PATH} --configuration Release --output ${FUNCTION_APP_PATH}
     
-    pushd "${INFRASTRUCTURE_PATH}/build/function_app"
-        zip -r "${INFRASTRUCTURE_PATH}/build/function_app.zip" ./*
+    pushd ${FUNCTION_APP_PATH}
+        zip -r ${FUNCTION_ZIP_PATH} ./*
     popd
      
 }
@@ -18,7 +21,7 @@ build_function_app() {
 deploy_infrastructure() {
     pushd ${INFRASTRUCTURE_PATH}/prod
         terraform init
-        terraform plan -out=deploy.tfplan
+        terraform plan -out=deploy.tfplan -var="function_app_path=${FUNCTION_ZIP_PATH}"
         terraform apply -input=false ./deploy.tfplan
     popd
 }
