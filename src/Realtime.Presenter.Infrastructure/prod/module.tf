efa17b984aa9feb51f33278aaf81f68e86f1f992
Bryce Klinker <bryce.klinker@gmail.com>
2020-04-07 15:19:58 -0500

    Updating to allow function_app_path to be passed from the command line

--- src/Realtime.Presenter.Infrastructure/prod/module.tf
+++ src/Realtime.Presenter.Infrastructure/prod/module.tf
@@ -1,9 +1,13 @@
+variable "function_app_path" {
+  type = string
+}
+
 module "realtime_presenter_prod" {
   source = "../core"
   
   env = "prod"
   app_name = "presenter"
-  function_app_path = "../build/function_app.zip"
+  function_app_path = var.function_app_path
   
   common_tags = {
     env = "prod"
