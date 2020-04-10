96ad6c52a6f673371b53b81d22d23010aecdbabb
Bryce Klinker <bryce.klinker@gmail.com>
2020-04-10 00:21:40 -0500

    Updating dynamodb to have capacities set

--- src/Realtime.Presenter.Infrastructure/core/dynamodb.tf
+++ src/Realtime.Presenter.Infrastructure/core/dynamodb.tf
@@ -2,6 +2,9 @@ resource "aws_dynamodb_table" "current_slide" {
 
   hash_key = "id"
   name = "Presentations"
+  billing_mode = "PROVISIONED"
+  read_capacity  = 20
+  write_capacity = 20
   
   attribute {
     name = "id"
